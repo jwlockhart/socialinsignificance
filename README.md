77bc38bcfc6f6a6095e11f48906343a7c7784b62
Jeff Lockhart <jwlockhart@users.noreply.github.com>
2015-06-10 00:21:13 +0100

    added helper.js

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ The live version that I'm tinkering with can be found here: http://storm.cis.for
 - [d3.js](http://d3js.org/) for all the pretty visuals
 - [errcw's gaussian repo](https://github.com/errcw/gaussian/) for some of the statistics
 - [jay3dec's d3 line chart tutorial](http://code.tutsplus.com/tutorials/building-a-multi-line-chart-using-d3js--cms-22935)
+- [this stackoverflow thread](https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript) for js url parsing
---
+++ js/helpers.js
@@ -0,0 +1,13 @@
+/**
+ * helper functions used in various parts of the site. Almost all borrowed and slightly modified from other sources.
+ */
+
+
+// js function for getting URL parameters
+// from https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
